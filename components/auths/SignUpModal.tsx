10c253dcd0fe9c74b6b0f4919a35d9e4031e56da
Uni <joseoyun12@gmail.com>
2023-02-15 18:08:34 +0900

    인풋 공통컴포넌트를 적용하고 값을 받고 변경할 수 있도록 코드 추가함

--- components/auths/SignUpModal.tsx
+++ components/auths/SignUpModal.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import CloseXIcon from '../../public/static/svg/auth/modal_close_x_icon.svg';
 import MailIcon from '../../public/static/svg/auth/mail.svg';
 import PersonIcon from '../../public/static/svg/auth/person.svg';
 import OpenedEyeIcon from '../../public/static/svg/auth/opened-eye.svg';
-import ClosedEyeIcon from '../../public/static/svg/auth/closed_eye.svg';
-import palette from '../../styles/palette';
+import Input from '../common/Input';
 
 const Container = styled.form`
 	width: 568px;
@@ -22,46 +21,68 @@ const Container = styled.form`
 	.input-wrapper {
 		position: relative;
 		margin-bottom: 16px;
-		input {
-			position: relative;
-			width: 100%;
-			height: 46px;
-			padding: 0 44px 0 11px;
-			border: 1px solid ${palette.gray_eb};
-			border-radius: 4px;
-			font-size: 16px;
-			outline: none;
-			::placeholder {
-				color: ${palette.gray_76};
-			}
-		}
-		svg {
-			position: absolute;
-			right: 11px;
-			top: 16px;
-		}
 	}
 `;
 
 const SignUpModal: React.FC = () => {
+	const [email, setEmail] = useState('');
+	const [lastname, setLastname] = useState('');
+	const [firstname, setFirstname] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setEmail(event.target.value);
+	};
+
+	const onChangeLastname = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setLastname(event.target.value);
+	};
+
+	const onChangeFirstname = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setFirstname(event.target.value);
+	};
+
+	const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(event.target.value);
+	};
+
 	return (
 		<Container>
 			<CloseXIcon className="mordal-close-x-icon" />
 			<div className="input-wrapper">
-				<input placeholder="이메일 주소" type="email" name="email" />
-				<MailIcon />
+				<Input
+					placeholder="이메일 주소"
+					type="email"
+					icon={<MailIcon />}
+					name="email"
+					value={email}
+					onChange={onChangeEmail}
+				/>
 			</div>
 			<div className="input-wrapper">
-				<input placeholder="이름(예:길동)" />
-				<PersonIcon />
+				<Input
+					placeholder="이름(예:길동)"
+					icon={<PersonIcon />}
+					value={lastname}
+					onChange={onChangeLastname}
+				/>
 			</div>
 			<div className="input-wrapper">
-				<input placeholder="성(예:홍)" />
-				<PersonIcon />
+				<Input
+					placeholder="성(예:홍)"
+					icon={<PersonIcon />}
+					value={firstname}
+					onChange={onChangeFirstname}
+				/>
 			</div>
 			<div className="input-wrapper">
-				<input placeholder="비밀번호 설정하기" type="password" />
-				<OpenedEyeIcon />
+				<Input
+					placeholder="비밀번호 설정하기"
+					type="password"
+					icon={<OpenedEyeIcon />}
+					value={password}
+					onChange={onChangePassword}
+				/>
 			</div>
 		</Container>
 	);
