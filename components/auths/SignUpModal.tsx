bcc295fadc7c51f5799c5d153a9c78aaa981bccf
Uni <joseoyun12@gmail.com>
2023-02-20 17:29:42 +0900

    생일에 대한 설명과 셀렉터 컴포넌트 적용함

--- components/auths/SignUpModal.tsx
+++ components/auths/SignUpModal.tsx
@@ -6,6 +6,9 @@ import PersonIcon from '../../public/static/svg/auth/person.svg';
 import OpenedEyeIcon from '../../public/static/svg/auth/opened-eye.svg';
 import ClosedEyeIcon from '../../public/static/svg/auth/closed_eye.svg';
 import Input from '../common/Input';
+import palette from '../../styles/palette';
+import Selector from '../common/Selector';
+import { monthList } from '../../lib/staticData';
 
 const Container = styled.form`
 	width: 568px;
@@ -28,6 +31,18 @@ const Container = styled.form`
 		svg {
 			cursor: pointer;
 		}
+	}
+
+	.sign-up-birthday-label {
+		font-size: 16px;
+		font-weight: 600;
+		margin-top: 16px;
+		margin-bottom: 8px;
+	}
+
+	.sign-up-modal-birthday-info {
+		margin-bottom: 16px;
+		color: ${palette.charcoal};
 	}
 `;
 
@@ -102,6 +117,16 @@ const SignUpModal: React.FC = () => {
 					onChange={onChangePassword}
 				/>
 			</div>
+			<p className="sign-up-birthday-label">생일</p>
+			<p className="sign-up-modal-birthday-info">
+				만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른
+				에어비엔비 이용자에게 공개되지 않습니다.
+			</p>
+			<Selector
+				options={['월', ...monthList]}
+				disabledOptions={['월']}
+				defaultValue="월"
+			/>
 		</Container>
 	);
 };
