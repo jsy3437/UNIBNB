b7a11d8019b095a510f9f983a66dfda700a781dd
Uni <joseoyun12@gmail.com>
2023-02-23 11:40:25 +0900

    회원가입 요청을 처리하는 파일생성함

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method === 'POST') {
+		return res.end();
+	}
+	res.statusCode = 405;
+
+	return res.end();
+};
