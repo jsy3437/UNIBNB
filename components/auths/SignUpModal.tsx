ed7cec341cbce1e234cd3f1e67a71bbdfa535da7
Uni <joseoyun12@gmail.com>
2023-02-23 11:24:36 +0900

    버튼 컴포넌트 적용함

--- components/auths/SignUpModal.tsx
+++ components/auths/SignUpModal.tsx
@@ -9,6 +9,7 @@ import Input from '../common/Input';
 import palette from '../../styles/palette';
 import Selector from '../common/Selector';
 import { dayList, monthList, yearList } from '../../lib/staticData';
+import Button from '../common/Button';
 
 const Container = styled.form`
 	width: 568px;
@@ -59,6 +60,12 @@ const Container = styled.form`
 		.sign-up-modal-birthday-year-selector {
 			width: 33.3333%;
 		}
+	}
+
+	.sign-up-modal-submit-button-wrapper {
+		margin-bottom: 16px;
+		padding-bottom: 16px;
+		border-bottom: 1px solid ${palette.gray_eb};
 	}
 `;
 
@@ -181,6 +188,9 @@ const SignUpModal: React.FC = () => {
 						onChange={onChangeBirthYear}
 					/>
 				</div>
+			</div>
+			<div className="sign-up-modal-submit-button-wrapper">
+				<Button type="submit">가입하기</Button>
 			</div>
 		</Container>
 	);
