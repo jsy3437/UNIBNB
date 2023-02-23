0d825ba3f13d3d399bb5431b9d08b161dd255b5c
Uni <joseoyun12@gmail.com>
2023-02-23 14:21:47 +0900

    비밀번호 암호화 적용 후 새로운 유저 추가하는 코드 작성함

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import Data from '../../../lib/data';
+import { StoredUserType } from '../../../types/user';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === 'POST') {
@@ -11,8 +14,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		const userExist = Data.user.exist({ email });
 		if (userExist) {
 			res.statusCode = 409;
-			res.send('이미 가입된 이메일입니다.');
+			return res.send('이미 가입된 이메일입니다.');
 		}
+
+		const hashedPassword = bcrypt.hashSync(password, 8);
+		const users = Data.user.getList();
+		let userId;
+		if (users.length === 0) {
+			userId = 1;
+		} else {
+			userId = users[users.length - 1].id + 1;
+		}
+		const newUser: StoredUserType = {
+			id: userId,
+			email,
+			firstname,
+			lastname,
+			password: hashedPassword,
+			birthday,
+			profileImage: '/static/image/default_user_profile_image.jpg',
+		};
+
+		Data.user.write([...users, newUser]);
+
 		return res.end();
 	}
 	res.statusCode = 405;
